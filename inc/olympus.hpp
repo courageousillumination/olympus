92d00453f08736c03c23b39e508f6b41066650f0
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-07-29 20:43:45 -0500

    Added cmake configured constants.

--- inc/olympus.hpp
+++ inc/olympus.hpp
@@ -1,5 +1,9 @@
 #ifndef OLYMPUS
 #define OLYMPUS
+
+#define OLYMPUS_VERSION_MAJOR 0
+#define OLYMPUS_VERSION_MINOR 1
+
 namespace olympus {
     void foo();
 }
--- src/olympus.cpp
+++ src/olympus.cpp
@@ -3,5 +3,5 @@
 #include "olympus.hpp"
 
 void olympus::foo() {
-    std::cout << "Hello world! Foo\n";
+    std::cout << "Hello world! Olympus " << OLYMPUS_VERSION_MAJOR << "." << OLYMPUS_VERSION_MINOR << std::endl;
 }
