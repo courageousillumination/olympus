343ee1bda44800efc9bccf92971e1469f7d74763
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-08-02 15:10:03 -0500

    Updated create_files script to create a test file.

--- src/olympus.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "olympus.hpp"
-
-int olympus::foo(int a, int b) {
-    return a + b;
-}
--- utils/create_files.py
+++ utils/create_files.py
@@ -14,17 +14,26 @@ if __name__ == "__main__":
     
     src_path = os.path.join("../src", directory)
     inc_path = os.path.join("../inc", directory)
+    test_path = os.path.join("../test", directory)
     
     if not os.path.exists(src_path):
         os.makedirs(src_path)
     if not os.path.exists(inc_path):
         os.makedirs(inc_path)
+    if not os.path.exists(test_path):
+        os.makedirs(test_path)
         
         
     #Create the directory if necessary
     src = open(os.path.join(src_path, file_name + ".cpp"), "w")
     src.write('#include "{0}"\n'.format(full_path + ".hpp"))
     src.close()
+    
+    #Create the test file
+    test = open(os.path.join(test_path, file_name + "_test.cpp"), "w")
+    #test.write('#include <gtest/gtest.h>\n\n#include "{0}"\n'.format(full_path + ".hpp"))
+    test.write('#include <gtest/gtest.h>\n\n#include "olympus.hpp"\n')
+    test.close()
     
     inc = open(os.path.join(inc_path, file_name + ".hpp"), "w")
     full_path = full_path.replace('/', '__')
@@ -34,3 +43,4 @@ if __name__ == "__main__":
     inc.write('#define {0}\n'.format(macro))
     inc.write('#endif\n')
     inc.close()
+    
