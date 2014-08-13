3631654c8ab5f4f223722253c509ce739b3b42b7
Tristan Rasmussen <tristanr93@gmail.com>
2014-08-12 20:55:09 -0500

    Removed highly variable fps test.

--- test/debug/fps_test.cpp
+++ test/debug/fps_test.cpp
@@ -20,11 +20,4 @@ TEST_F(FPSTest, FPSTest) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     fps::fps_tick();
     EXPECT_TRUE(fps::get_fps() > 8.0 && fps::get_fps() < 12.0);
-    
-    for (unsigned i = 0; i < 100; i++) {
-        fps::fps_tick();
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    fps::fps_tick();
-    EXPECT_TRUE(fps::get_fps() > 505.0 && fps::get_fps() < 510.0);
 }
