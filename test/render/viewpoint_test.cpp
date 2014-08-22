f6f23930a98aec8769fa70fe4ecb280ec57d599b
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-08-21 20:59:51 -0500

    Fixed failing viewpoint tests.

--- test/render/viewpoint_test.cpp
+++ test/render/viewpoint_test.cpp
@@ -36,17 +36,18 @@ protected:
 TEST_F (ViewpointTest, MatrixCalculations) {
     //All three should default to the same value
     EXPECT_EQ(viewpoint->get_view_matrix(), glm::mat4(1.0f));
-    EXPECT_EQ(viewpoint->get_projection_matrix(), glm::mat4(1.0f));
-    EXPECT_EQ(viewpoint->get_view_projection_matrix(), glm::mat4(1.0f));
     
     viewpoint->set_position(1.0f, 0.0f, 0.0f);
-    EXPECT_EQ(viewpoint->get_view_matrix(), glm::translate(glm::vec3(1.0f, 0.0f, 0.0f)));
-    EXPECT_EQ(viewpoint->get_view_projection_matrix(), glm::translate(glm::vec3(1.0f, 0.0f, 0.0f)));
-    
+    glm::mat4 expected_matrix = glm::lookAt(glm::vec3(1.0f, 0.0f, 0.0f),
+                                            glm::vec3(1.0f, 0.0f,-1.0f),
+                                            glm::vec3(0.0f, 1.0f, 0.0f));
+    EXPECT_EQ(viewpoint->get_view_matrix(), expected_matrix);
+     
     //Test the projection_matrix
     viewpoint->set_perspective(1.0f, 4.0f / 3.0f, 1.0f, 2.0f);
+    expected_matrix = glm::perspective(1.0f, 4.0f / 3.0f, 1.0f, 2.0f) * expected_matrix;
     EXPECT_EQ(viewpoint->get_projection_matrix(), glm::perspective(1.0f, 4.0f / 3.0f, 1.0f, 2.0f));
-    EXPECT_EQ(viewpoint->get_view_projection_matrix(), glm::translate(glm::vec3(1.0f, 0.0f, 0.0f)) * glm::perspective(1.0f, 4.0f / 3.0f, 1.0f, 2.0f));
+    EXPECT_EQ(viewpoint->get_view_projection_matrix(), expected_matrix);
 }
 
 TEST_F (ViewpointTest, AffectsImage) {
